fa6b5bcfca9c9dec6fd58e3d6bc84885aa2f9b53
Siyuan Ma <smy20011@gmail.com>
2024-12-27 22:27:58 -0800

    Enable Graph

--- src/lib/Trend.svelte
+++ src/lib/Trend.svelte
@@ -7,27 +7,32 @@
     let div: HTMLElement | undefined = $state();
     let data_with_ci = $derived.by(() => {
         return fsd_version_data.map(d => {
-            let failure = Math.round((1 - d.success_rate) * d.runs)
-            let low = 2 * d.city_miles / chisquare.quantile(confidence / 2, 2 * failure + 2);
-            let high = 2 * d.city_miles / chisquare.quantile(1 - confidence / 2, 2 * failure);
+            let failure = Math.round((1 - d.success_rate) * d.runs);
+            let alpha = 1 - confidence
+            let low = 2 * d.city_miles / chisquare.quantile(1 - alpha / 2, 2 * failure + 2);
+            let high = 2 * d.city_miles / chisquare.quantile(alpha / 2, 2 * failure);
             return {
                 ...d,
                 low,
                 high,
                 mttf: d.city_miles / failure,
+                failure,
             }
-        });
+        }).filter(d => d.failure > 5);
     });
 
     $effect(() => {
         div?.firstChild?.remove(); // remove old chart, if any
+        console.log(data_with_ci);
         div?.append(Plot.plot({
             marks: [
-                Plot.areaY(data_with_ci, {x: "min_date", y1: "low", y2: "high"}),
-                Plot.line(data_with_ci, {x: "min_date", y: "mttf"})
-            ]
+                Plot.areaY(data_with_ci, {x: "min_date", y1: "low", y2: "high", fillOpacity: 0.3}),
+                Plot.line(data_with_ci, {x: "min_date", y: "mttf"}),
+                Plot.dot(data_with_ci, {x: "min_date", y: "mttf", tip: true, channels: {version: "version", "MTTF": "mttf", "Lower Bound": "low", "Upper Bound": "high"}})
+            ],
+            grid: true,
         })); // add the new chart
     });
 </script>
 
-<div bind:this={div} role="img"></div>
+<div bind:this={div} role="img" style="place-items: center"></div>
